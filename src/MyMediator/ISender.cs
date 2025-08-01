a8c7e55834d63f74b827326bd8d731b75e956244
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-08-01 15:46:11 -0700

    Add `ISender` interface to `MyMediator` project

---
+++ src/MyMediator/ISender.cs
@@ -0,0 +1,17 @@
+﻿// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     ISender.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  MyMediator
+// =======================================================
+
+namespace MyMediator;
+
+public interface ISender
+{
+
+	Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+
+}
