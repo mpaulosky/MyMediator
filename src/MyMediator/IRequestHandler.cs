f188fd06629f3e6ad132f37cdd7d1263914634df
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-08-01 15:45:59 -0700

    Add `IRequest` and `IRequestHandler` interfaces to `MyMediator` project

---
+++ src/MyMediator/IRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     IRequestHandler.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  MyMediator
+// =======================================================
+
+namespace MyMediator;
+
+public interface IRequestHandler<TRequest, TResponse>
+		where TRequest : IRequest<TResponse>
+{
+
+	Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+
+}
