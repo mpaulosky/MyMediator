db85aa870d81381ae5615794b3414ca584865ba0
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-08-01 15:45:51 -0700

    Add `IRequest` interface and `GlobalUsings.cs` to `MyMediator` project

---
+++ src/MyMediator/IRequest.cs
@@ -0,0 +1,12 @@
+﻿// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     IRequest.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  MyMediator
+// =======================================================
+
+namespace MyMediator;
+
+public interface IRequest<TResponse> { }
