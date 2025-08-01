1de8783c9a3e84c867dfb4b0b87e2369065eb612
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-08-01 15:47:20 -0700

    Add `Sender` class with `Send` method implementation to `MyMediator` project

---
+++ src/MyMediator/Sender.cs
@@ -0,0 +1,26 @@
+﻿// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     Sender.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  MyMediator
+// =======================================================
+
+namespace MyMediator;
+
+public class Sender
+(
+		IServiceProvider provider
+) : ISender
+{
+
+	public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+	{
+		var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+		dynamic handler = provider.GetRequiredService(handlerType);
+
+		return handler.Handle((dynamic)request, cancellationToken);
+	}
+
+}
